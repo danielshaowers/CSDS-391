1e0cbe74318ff59fd3b75119d4ad763e5b36d566
Daniel Shao <50784866+danielshaowers@users.noreply.github.com>
2020-02-27 20:14:16 -0500

    improved utility and heuristic functions

---
+++ minimax/minimax/MapLocation.java
@@ -0,0 +1,51 @@
+package minimax;
+
+public class MapLocation implements Comparable<MapLocation> {
+    public int x, y;
+    MapLocation cameFrom; //the node leading to our current
+    float cost; //cost represents the heuristic value
+    public int parents;
+
+    public MapLocation(int x, int y, MapLocation cameFrom, float cost, int parentCount)
+    {
+        this.x = x;
+        this.y = y;
+        this.cost = cost;
+        this.cameFrom = cameFrom;
+        this.parents = parentCount;
+    }
+    
+    //calculates euclidean distance given the current node and goal node
+   	public static int calculateManhattan(MapLocation current, MapLocation goal) {
+   		return Math.abs(current.x - goal.x) + Math.abs(current.y - goal.y);	
+   	}
+   	
+    @Override
+    public boolean equals(Object ml) {
+    	return ml instanceof MapLocation && this.x == ((MapLocation)ml).x 
+    			&& this.y == ((MapLocation)ml).y;
+    }
+    
+    public float getCost() {
+    	return cost;
+    }
+    
+    public int getParentCount() {
+    	return parents;
+    }
+    
+    @Override //hash code using bijective theorem
+    public int hashCode() {
+    	int tmp = ( y +  ((x+1)/2));
+        return x +  ( tmp * tmp);
+    }
+    
+    @Override
+    public int compareTo(MapLocation compare) {
+    	if (compare.getCost() < this.cost)
+    		return 1;
+    	if (compare.getCost() > this.cost)
+    		return -1;
+    	return 0;
+    }
+}
