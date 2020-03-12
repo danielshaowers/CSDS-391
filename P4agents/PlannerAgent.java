3e5db5c6b94acd9c936ff58519c0d398f6afd0df
deepster <59594456+deepster@users.noreply.github.com>
2020-03-12 18:25:12 -0500

    Add files via upload
    rough draft, edited GameState (constructors, isgoal() and getChildren()), StripsActions, and PEAgent (Astarsearch)

---
+++ P4agents/PlannerAgent.java
@@ -0,0 +1,166 @@
+package EECS391_sepia;
+
+
+import edu.cwru.sepia.action.Action;
+import edu.cwru.sepia.agent.Agent;
+import edu.cwru.sepia.environment.model.history.History;
+import edu.cwru.sepia.environment.model.state.State;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Created by Devin on 3/15/15.
+ */
+public class PlannerAgent extends Agent {
+
+    final int requiredWood;
+    final int requiredGold;
+    final boolean buildPeasants;
+    Stack<StripsAction> path = new Stack<StripsAction>();
+
+    // Your PEAgent implementation. This prevents you from having to parse the text file representation of your plan.
+    PEAgent peAgent;
+
+    public PlannerAgent(int playernum, String[] params) {
+        super(playernum);
+
+        if(params.length < 3) {
+            System.err.println("You must specify the required wood and gold amounts and whether peasants should be built");
+        }
+
+        requiredWood = Integer.parseInt(params[0]);
+        requiredGold = Integer.parseInt(params[1]);
+        buildPeasants = Boolean.parseBoolean(params[2]);
+
+
+        System.out.println("required wood: " + requiredWood + " required gold: " + requiredGold + " build Peasants: " + buildPeasants);
+    }
+
+    @Override
+    public Map<Integer, Action> initialStep(State.StateView stateView, History.HistoryView historyView) {
+
+        Stack<StripsAction> plan = AstarSearch(new GameState(stateView, playernum, requiredGold, requiredWood, buildPeasants));
+
+        if(plan == null) {
+            System.err.println("No plan was found");
+            System.exit(1);
+            return null;
+        }
+
+        // write the plan to a text file
+        savePlan(plan);
+
+
+        // Instantiates the PEAgent with the specified plan.
+        peAgent = new PEAgent(playernum, plan);
+
+        return peAgent.initialStep(stateView, historyView);
+    }
+
+    @Override
+    public Map<Integer, Action> middleStep(State.StateView stateView, History.HistoryView historyView) {
+        if(peAgent == null) {
+            System.err.println("Planning failed. No PEAgent initialized.");
+            return null;
+        }
+
+        return peAgent.middleStep(stateView, historyView);
+    }
+
+    @Override
+    public void terminalStep(State.StateView stateView, History.HistoryView historyView) {
+
+    }
+
+    @Override
+    public void savePlayerData(OutputStream outputStream) {
+
+    }
+
+    @Override
+    public void loadPlayerData(InputStream inputStream) {
+
+    }
+
+    /**
+     * Perform an A* search of the game graph. This should return your plan as a stack of actions. This is essentially
+     * the same as your first assignment. The implementations should be very similar. The difference being that your
+     * nodes are now GameState objects not MapLocation objects.
+     *
+     * @param startState The state which is being planned from
+     * @return The plan or null if no plan is found.
+     */
+    private Stack<StripsAction> AstarSearch(GameState startState) {
+    	path.clear(); //want to empty Stack every time Astar is called
+    	Hashtable<Integer, GameState> closed = new Hashtable<Integer, GameState>(); //this becomes unnecessary?
+    	PriorityQueue<GameState> open = new PriorityQueue<GameState>(); //tracks any potential nodes
+    	List<GameState> children = null;
+        open.add(startState);
+        while (open.size() != 0) { 
+        	GameState current = open.poll(); //get the cheapest node   	     
+        	if (current.isGoal()) //if the goal is found
+        		return tracePath(current.camefrom); //helper method that traces from goal	
+        	closed.putIfAbsent(current.hashCode(), current); //add current to closed list
+        	children = current.generateChildren();	            
+        	for(GameState child : children)
+        	{
+        		GameState hashVal = closed.get(child.hashCode());
+    			if (hashVal == null || (child.equals(hashVal) && child.cost < hashVal.cost)) //if current has never been visited, or if cheaper than what's already visited
+    					open.add(child); 	    //adds to open list if temp is cheaper than other paths to temp in closed list
+
+    			}
+        	}
+    		return path;  
+
+        }
+    
+    //trace back the path from the goal node		
+    public Stack<StripsAction> tracePath(StripsAction goal) {
+    	Stack<StripsAction> path = new Stack<StripsAction>();
+    	for (StripsAction parent = goal.getCameFrom(); parent.getCameFrom() != null; parent = parent.getCameFrom()) { 
+    		path.push(parent);
+    	}
+    	return path;
+    }
+
+    /**
+     * This has been provided for you. Each strips action is converted to a string with the toString method. This means
+     * each class implementing the StripsAction interface should override toString. Your strips actions should have a
+     * form matching your included Strips definition writeup. That is <action name>(<param1>, ...). So for instance the
+     * move action might have the form of Move(peasantID, X, Y) and when grounded and written to the file
+     * Move(1, 10, 15).
+     *
+     * @param plan Stack of Strips Actions that are written to the text file.
+     */
+    private void savePlan(Stack<StripsAction> plan) {
+        if (plan == null) {
+            System.err.println("Cannot save null plan");
+            return;
+        }
+
+        File outputDir = new File("saves");
+        outputDir.mkdirs();
+
+        File outputFile = new File(outputDir, "plan.txt");
+
+        PrintWriter outputWriter = null;
+        try {
+            outputFile.createNewFile();
+
+            outputWriter = new PrintWriter(outputFile.getAbsolutePath());
+
+            Stack<StripsAction> tempPlan = (Stack<StripsAction>) plan.clone();
+            while(!tempPlan.isEmpty()) {
+                outputWriter.println(tempPlan.pop().toString());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (outputWriter != null)
+                outputWriter.close();
+        }
+    }
+}
---
+++ P4agents/StripsAction.java
@@ -0,0 +1,182 @@
+package EECS391_sepia;
+
+import edu.cwru.sepia.action.Action;
+import edu.cwru.sepia.environment.model.state.State;
+
+/**
+ * A useful start of an interface representing strips actions. You may add new methods to this interface if needed, but
+ * you should implement the ones provided. You may also find it useful to specify a method that returns the effects
+ * of a StripsAction.
+ */
+public interface StripsAction {
+
+    /**
+     * Returns true if the provided GameState meets all of the necessary conditions for this action to successfully
+     * execute.
+     *
+     * As an example consider a Move action that moves peasant 1 in the NORTH direction. The partial game state might
+     * specify that peasant 1 is at location (3, 3). In this case the game state shows that nothing is at location (3, 2)
+     * and (3, 2) is within bounds. So the method returns true.
+     *
+     * If the peasant were at (3, 0) this method would return false because the peasant cannot move to (3, -1).
+     *
+     * @param state GameState to check if action is applicable
+     * @return true if apply can be called, false otherwise
+     */
+    public boolean preconditionsMet(GameState state);
+
+    /**
+     * Applies the action instance to the given GameState producing a new GameState in the process.
+     *
+     * As an example consider a Move action that moves peasant 1 in the NORTH direction. The partial game state
+     * might specify that peasant 1 is at location (3, 3). The returned GameState should specify
+     * peasant 1 at location (3, 2).
+     *
+     * In the process of updating the peasant state you should also update the GameState's cost and parent pointers.
+     *
+     * @param state State to apply action to
+     * @return State resulting from successful action appliction.
+     */
+    public GameState apply(GameState state);
+    
+    //returns the StripsAction the lead to the new gamestate
+    public StripsAction getCameFrom();
+}
+
+//Strips action classes that implement interface
+class moveTo implements StripsAction{
+	Position agentpos;
+	Position locationpos;
+	int agentId;
+	int locationId;
+	StripsAction camefrom;
+	
+	
+	//Initializes agents position and location the agent wants go to 
+	public moveTo(Position agent, Position location, int agId, int locId, GameState state)
+	{
+		agentpos = agent;
+		locationpos = location;
+		this.agentId = agId;
+		locationId = locId;
+		camefrom = state.camefrom;
+	}
+	@Override
+	//determines if agent position is equal to location position
+	public boolean preconditionsMet(GameState state) {
+		if(!agentpos.equals(locationpos))
+			return true;
+		return false;
+	}
+	@Override
+	public GameState apply(GameState state) {
+		Position agentposnew = agentpos.move(agentpos.getDirection(locationpos));
+		return new GameState(agentposnew, state.currentGold, state.currentWood, state.goldheld,state.woodheld, 
+				new moveTo(agentpos,locationpos,agentId,locationId, state), state);
+	}
+	//returns cost to get from agent position to desired location
+	public double getCost(){
+		return agentpos.euclideanDistance(locationpos);
+	}
+	
+	@Override
+	//returns StripsAction took to get to this state
+	public StripsAction getCameFrom() {
+		return camefrom;
+	}
+}
+
+class harvest implements StripsAction{
+	Position agentpos;
+	Position locationpos;
+	int agentId;
+	int locationId;
+	StripsAction camefrom;
+
+	//Initializes agents position and location the agent wants go to 
+	public harvest(Position agent, Position location, int agId, int locId, GameState state)
+	{
+		this.agentpos = agent;
+		this.locationpos = location;
+		this.agentId = agId;
+		locationId = locId;
+		camefrom = state.camefrom;
+	}
+	@Override
+	public boolean preconditionsMet(GameState state) {
+		return (!state.isGoal() && agentpos.isAdjacent(locationpos)); //makes sure state isn't goal state and that the agent is next to harvest location
+	}
+	@Override
+	public GameState apply(GameState state) {
+		Action gather = Action.createPrimitiveGather(agentId, agentpos.getDirection(locationpos)); //
+		if(location(state)) { //determine where it collected the resource from
+			
+			int gathered = state.state.getUnit(gather.getUnitId()).getTemplateView().getGoldCost(); //how much gold collected after action is executed 
+			//returns new GameState with updated class variables and stripsAction that led to this GameState
+			return new GameState(agentpos, state.currentGold, state.currentWood, gathered,state.woodheld, 
+					new harvest(agentpos,locationpos,agentId,locationId,state), state);
+		}
+		else {
+			int gathered = state.state.getUnit(gather.getUnitId()).getTemplateView().getWoodCost(); //how much wood collected after action is executed 
+			//returns new GameState with updated class variables and stripsAction that led to this GameState
+			return new GameState(agentpos, state.currentGold, state.currentWood,state.goldheld, gathered, 
+					new harvest(agentpos,locationpos,agentId,locationId,state), state);
+		}	
+	}
+	//determines if its a goldmine or a tree   might try and condense this with above
+	public boolean location(GameState state){ 
+		for(int loc : state.goldmines)
+		{
+			if(loc == locationId)
+				return true;
+		}
+		return false;
+	}
+	@Override
+	//returns StripsAction took to get to this state
+	public StripsAction getCameFrom() {
+		return camefrom;
+	}
+	
+}
+
+
+class deposit implements StripsAction{
+	Position agentpos;
+	Position locationpos;
+	int agentId;
+	int locationId;
+	StripsAction camefrom;
+	//Initializes agents position and location the agent wants go to 
+	public deposit(Position agent, Position location, int agId, int locId, GameState state)
+	{
+		this.agentpos = agent;
+		this.locationpos = location;
+		this.agentId = agId;
+		this.locationId = locId;
+		camefrom = state.camefrom;
+	}
+	@Override
+	public boolean preconditionsMet(GameState state) {
+		return (state.isGoal() && agentpos.isAdjacent(locationpos));
+	}
+
+	@Override
+	public GameState apply(GameState state) {
+		Action deposit = Action.createPrimitiveGather(agentId, agentpos.getDirection(locationpos));
+		int deposited = state.state.getUnit(deposit.getUnitId()).getTemplateView().getGoldCost(); //how much resource was deposited 
+		if(deposited ==0)  //if its zero then no gold was deposited
+			return new GameState(agentpos, state.currentGold, deposited,state.goldheld, state.woodheld, 
+					new harvest(agentpos,locationpos,agentId,locationId,state), state);				// returns state that has deposited wood
+		return new GameState(agentpos, deposited, state.currentWood,state.goldheld, state.woodheld, 
+				new harvest(agentpos,locationpos,agentId,locationId,state), state);					// returns state that has deposited gold
+	}
+	@Override
+	//returns StripsAction took to get to this state
+	public StripsAction getCameFrom() {
+		return camefrom;
+	}
+	
+}
+
+
